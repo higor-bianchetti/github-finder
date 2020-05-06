5ee94e62b9c0c0c6a9ec634b51dcb4e6f9ffcb41
Higor Bianchetti <higorbianchetti@gmail.com>
2020-05-06 20:57:25 -0300

    Add componente para exibição do usuário.

---
+++ src/components/users/User.js
@@ -0,0 +1,121 @@
+import React, { Fragment, Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import Repos from '../repos/Repos';
+import Loading from '../layout/Loading';
+
+class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      company,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable,
+    } = this.props.user;
+
+    const { repos, loading } = this.props;
+
+    if (loading === true) return <Loading />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to home
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success'></i>
+        ) : (
+          <i className='fas fa-times text-danger'></i>
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt={login}
+              className='round-img'
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a
+              href={html_url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='btn btn-primary my-1'>
+              Visit Github profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+          <div className='badge badge-warning'>
+            Public Gists: {public_gists}
+          </div>
+        </div>
+        <Repos repos={repos} />
+      </Fragment>
+    );
+  }
+}
+
+export default User;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
@@ -12,13 +13,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a
-          href={html_url}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='btn btn-primary btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-primary btn-sm my-1'>
           View
-        </a>
+        </Link>
       </div>
     </div>
   );
